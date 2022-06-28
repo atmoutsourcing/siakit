dea17a6f3a8b29a4117935199f592a3767b07eea
Bruno Fabre <bruno.hfabre@gmail.com>
2022-06-28 11:03:55 -0300

    add card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,17 @@
+import React, { forwardRef, ReactNode } from 'react';
+
+import { Container } from './styles';
+
+type CardProps = {
+  children: ReactNode;
+};
+
+export const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <Container ref={ref} {...rest}>
+        {children}
+      </Container>
+    );
+  },
+);
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.cardBackground};
+  box-shadow: ${({ theme }) => theme.shadows.sm};
+  border: 1px solid ${({ theme }) => theme.colors.gray[3]};
+`;
