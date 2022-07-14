9740f4967a797272a3af85da625dc9c044effd03
= <=>
2022-07-14 11:14:47 -0300

    change version

--- src/helpers/icons.ts
+++ src/helpers/icons.ts
@@ -1,22 +1,45 @@
-export * from 'react-icons/ai';
-export * from 'react-icons/bi';
-export * from 'react-icons/bs';
-export * from 'react-icons/cg';
-export * from 'react-icons/di';
-export * from 'react-icons/fa';
-export * from 'react-icons/fc';
-export * from 'react-icons/fi';
-export * from 'react-icons/gi';
-export * from 'react-icons/go';
-export * from 'react-icons/gr';
-export * from 'react-icons/hi';
-export * from 'react-icons/im';
-export * from 'react-icons/io';
-export * from 'react-icons/md';
-export * from 'react-icons/ri';
-export * from 'react-icons/si';
-export * from 'react-icons/tb';
-export * from 'react-icons/ti';
-export * from 'react-icons/vsc';
+import * as ai from 'react-icons/ai';
+import * as bi from 'react-icons/bi';
+import * as bs from 'react-icons/bs';
+import * as cg from 'react-icons/cg';
+import * as di from 'react-icons/di';
+import * as fa from 'react-icons/fa';
+import * as fc from 'react-icons/fc';
+import * as fi from 'react-icons/fi';
+import * as gi from 'react-icons/gi';
+import * as go from 'react-icons/go';
+import * as gr from 'react-icons/gr';
+import * as hi from 'react-icons/hi';
+import * as im from 'react-icons/im';
+import * as io from 'react-icons/io';
+import * as md from 'react-icons/md';
+import * as ri from 'react-icons/ri';
+import * as si from 'react-icons/si';
+import * as tb from 'react-icons/tb';
+import * as ti from 'react-icons/ti';
+import * as vsc from 'react-icons/vsc';
+import * as wi from 'react-icons/wi';
 
-export * from 'react-icons/wi';
+export const icons = {
+  ...ai,
+  ...bi,
+  ...bs,
+  ...cg,
+  ...di,
+  ...fa,
+  ...fc,
+  ...fi,
+  ...gi,
+  ...go,
+  ...gr,
+  ...hi,
+  ...im,
+  ...io,
+  ...md,
+  ...ri,
+  ...si,
+  ...tb,
+  ...ti,
+  ...vsc,
+  ...wi,
+};
